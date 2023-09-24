4121f28bc704dd279a8f50d4bc2890c9adfa4480
Milton0222 <francimilton1997@gmail..com>
2023-09-24 13:05:27 +0100

    instalando o jeststream

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,15 @@
-<table class="table">
-<thead>
-    <th>nome</th><th>genero</th>
-    <tbody>
-        <tr>
-            <td>dfg</td>
-            <td>fgh</td>
-        </tr>
-    </tbody>
-</thead>
-</table>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <x-welcome />
+            </div>
+        </div>
+    </div>
+</x-app-layout>
