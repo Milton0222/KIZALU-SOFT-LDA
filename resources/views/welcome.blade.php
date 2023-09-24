5b4aa4f4d5afe33f30462006c66cfb26ba00f643
Milton0222 <francimilton1997@gmail..com>
2023-09-24 11:09:00 +0100

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Treinamento Taillwind</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <div class="bg-slate-600 flex-row gap-4 grid-flow-row text-red-600 text-9xl ">
+        Ola mundo
+       
+        <div class=" rounded-full bg-red-600 h-10 w-10 ">  </div>
+    <div class="rounded-full bg-green-600 h-10 w-10 "></div>
+    <div class="rounded-full bg-yellow-600 h-10 w-10"></div>
+
+    
+    </div>
+    <button class="bg-blue-500 text-white py-2 px-4 rounded-md ">entrar</button>
+    
+</body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./resources/**/*.blade.php",
+  "./resources/**/*.js",
+  "./resources/**/*.vue",
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
