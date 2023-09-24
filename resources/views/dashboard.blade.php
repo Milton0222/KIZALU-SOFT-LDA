dd22294427ed0946f763c2585f92870687ea2769
Milton0222 <francimilton1997@gmail..com>
2023-09-24 11:45:48 +0100

    refactor.alterando a home

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,11 @@
-<p>ola KIZALU</p>
+<table class="table">
+<thead>
+    <th>nome</th><th>genero</th>
+    <tbody>
+        <tr>
+            <td>dfg</td>
+            <td>fgh</td>
+        </tr>
+    </tbody>
+</thead>
+</table>
