db2dce134d211fbd397c214d4fb9089c049a22fb
Plácido Nguli <placidonguli20@gmail.com>
2023-10-05 15:40:04 +0800

    retificando rota

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::ge('/ver', function(){
+Route::get('/ver', function(){
     dd('ola kizalu');
 });
 Route::middleware([
