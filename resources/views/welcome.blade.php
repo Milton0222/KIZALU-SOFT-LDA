e6f95c20ac826408a158b9fe972536e5b6c98231
Milton0222 <francimilton1997@gmail..com>
2023-10-07 10:26:30 +0100

    alterando a tela de boas vinda

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,20 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Treinamento Taillwind</title>
+    <title>{{config('app.name','KIZALU SOFT-LDA')}}</title>
     @vite('resources/css/app.css')
 </head>
 <body>
     <div class="bg-slate-600 flex-row gap-4 grid-flow-row text-red-600 text-9xl ">
-        Ola mundo
+        KIZALU SOFT,LDA
        
-        <div class=" rounded-full bg-red-600 h-10 w-10 ">  </div>
+    <div class=" rounded-full bg-red-600 h-10 w-10 row-auto ">  </div>
     <div class="rounded-full bg-green-600 h-10 w-10 "></div>
     <div class="rounded-full bg-yellow-600 h-10 w-10"></div>
 
     
     </div>
-    <button class="bg-blue-500 text-white py-2 px-4 rounded-md ">entrar</button>
+   <a href="{{route('login')}}" target="_blank" rel="noopener noreferrer">
+   <button class="bg-blue-500 text-white py-2 px-4 rounded-md ">entrar</button>
+   </a> 
     
 </body>
 </html>
