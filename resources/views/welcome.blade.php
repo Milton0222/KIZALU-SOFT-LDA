b45b5ae7aaa8a32f5ad38a4b64e705387915451e
Plácido Nguli <placidonguli20@gmail.com>
2023-10-12 16:38:31 +0800

    ffff

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,16 +11,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-    <div class="bg-slate-600 flex-row gap-4 grid-flow-row text-red-600 text-9xl ">
-        Ola mundo
-       
-        <div class=" rounded-full bg-red-600 h-10 w-10 ">  </div>
-    <div class="rounded-full bg-green-600 h-10 w-10 "></div>
-    <div class="rounded-full bg-yellow-600 h-10 w-10"></div>
 
-    
-    </div>
-    <button class="bg-blue-500 text-white py-2 px-4 rounded-md ">entrar</button>
-    
 </body>
 </html>
