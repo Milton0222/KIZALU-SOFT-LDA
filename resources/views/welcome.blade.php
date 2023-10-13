c16e59b06940d24f800d6691fdee216ec4a9240d
Milton0222 <francimilton1997@gmail..com>
2023-10-13 16:44:06 +0100

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,18 +7,7 @@
     @vite('resources/css/app.css')
 </head>
 <body>
-    <div class="bg-slate-600 flex-row gap-4 grid-flow-row text-red-600 text-9xl ">
-        KIZALU SOFT,LDA
-       
-    <div class=" rounded-full bg-red-600 h-10 w-10 row-auto ">  </div>
-    <div class="rounded-full bg-green-600 h-10 w-10 "></div>
-    <div class="rounded-full bg-yellow-600 h-10 w-10"></div>
-
     
-    </div>
-   <a href="{{route('login')}}" target="_blank" rel="noopener noreferrer">
-   <button class="bg-blue-500 text-white py-2 px-4 rounded-md ">Login</button>
-   </a> 
     
 </body>
 </html>
